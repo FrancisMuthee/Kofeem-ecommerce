2c837cccde522dc8c362a76bdf2a3e50c02a0a8a
0757377860 <francisnjaramba2@gmail.com>
2024-02-10 13:37:04 +0300

    regular updates

--- Coffee/database/views.py
+++ Coffee/database/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
+def home(request):
+    return redirect('/')
+
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
